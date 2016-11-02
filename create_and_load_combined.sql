1efbdb24e9d033c45fa62ff2cf6f24b7861e9b0a
anjaanmd <mohandoss.ez@gmail.com>
2016-11-03 04:54:09 +0530

    added 'optionally enclosed by quote' in load data infile procedure

--- create_and_load_combined.sql
+++ create_and_load_combined.sql
@@ -73,12 +73,12 @@ CREATE TABLE combined_bowling(
 
 
 LOAD DATA INFILE '/var/lib/mysql-files/combined_t20.match_details.csv' INTO TABLE combined_match_details 
- FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+ FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
-LOAD DATA INFILE '/var/lib/mysql-files/combined_t20.batting.csv' INTO TABLE  combined_batting FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/combined_t20.batting.csv' INTO TABLE  combined_batting FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
 
-LOAD DATA INFILE '/var/lib/mysql-files/combined_t20.bowling.csv' INTO TABLE  combined_bowling FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/combined_t20.bowling.csv' INTO TABLE  combined_bowling FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
-LOAD DATA INFILE '/var/lib/mysql-files/combined_t20.score_details.csv' INTO TABLE  combined_score_details FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/combined_t20.score_details.csv' INTO TABLE  combined_score_details FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
